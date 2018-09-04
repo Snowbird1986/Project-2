4d3605169fcea20f39c76aded9ad6dfe79734476
Snowbird <jakounter@yahoo.com>
2018-09-04 18:15:05 -0500

    A

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   syncOptions.force = true;
 }
 
